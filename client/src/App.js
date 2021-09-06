a2ae7f2f35317f4f56480ed13b15fe0e402b5a87
joealsaaidah <alsaaidahyousef@gmail.com>
2021-09-06 23:27:18 +0800

    add the register & login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import Register from "./pages/Register";
 import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <Product />
+      <Login />
     </div>
   );
 }
